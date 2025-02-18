4fec6b289c730a1b7fd2234d92c10d6d9468a7ef
Mikołaj <kaleta222@gmail.com>
2025-02-18 09:35:30 +0100

    ui: update flags appearance
    - refactored retuenFlag function
    - flags now have a border and aspect ratio = 3/2

--- utils/functions.tsx
+++ utils/functions.tsx
@@ -1,4 +1,4 @@
-import { Image } from 'react-native'
+import { Image, StyleSheet } from 'react-native'
 
 import { foreignLanguages } from '@/constants/Types'
 
@@ -6,16 +6,55 @@ import { foreignLanguages } from '@/constants/Types'
 export default function returnFlag(lang: foreignLanguages) {
   switch (lang) {
     case 'en':
-      return <Image source={require(`@/assets/flags/en.png`)}></Image>
+      return (
+        <Image
+          style={styles.image}
+          source={require(`@/assets/flags/en.png`)}
+        ></Image>
+      )
     case 'pl':
-      return <Image source={require(`@/assets/flags/pl.png`)}></Image>
+      return (
+        <Image
+          style={styles.image}
+          source={require(`@/assets/flags/pl.png`)}
+        ></Image>
+      )
     case 'es':
-      return <Image source={require(`@/assets/flags/es.png`)}></Image>
+      return (
+        <Image
+          style={styles.image}
+          source={require(`@/assets/flags/es.png`)}
+        ></Image>
+      )
     case 'it':
-      return <Image source={require(`@/assets/flags/it.png`)}></Image>
+      return (
+        <Image
+          style={styles.image}
+          source={require(`@/assets/flags/it.png`)}
+        ></Image>
+      )
     case 'de':
-      return <Image source={require(`@/assets/flags/de.png`)}></Image>
+      return (
+        <Image
+          style={styles.image}
+          source={require(`@/assets/flags/de.png`)}
+        ></Image>
+      )
     default:
-      return <Image source={require(`@/assets/flags/en.png`)}></Image>
+      return (
+        <Image
+          style={styles.image}
+          source={require(`@/assets/flags/en.png`)}
+        ></Image>
+      )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    borderRadius: 3,
+    aspectRatio: 3 / 2,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,.1)',
+  },
+})

a33d15450f32e83934d7d7ecc685f275ee4ac8ef
Mikołaj <kaleta222@gmail.com>
2025-02-23 17:43:19 +0100

    refactor: split SettingsScreen into smaller components

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,87 +1,17 @@
-import Feather from '@expo/vector-icons/Feather'
 import { useEffect } from 'react'
-import { Pressable, ScrollView, StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 
-import Text from '@/components/texts'
+import ForeignLanguages from '@/components/settings/ForeignLanguages'
+import HomeLanguages from '@/components/settings/HomeLanguages'
+import ToggleTheme from '@/components/settings/ToggleTheme'
 import Colors from '@/constants/Colors'
 import {
   COLOR_THEME_STORAGE_KEY,
-  FAV_LANGUAGES_STORAGE_KEY,
   HOME_LANGUAGE_STORAGE_KEY,
 } from '@/constants/StorageKeys'
-import { translations } from '@/constants/Translations'
-import { foreignLanguages, homeLanguages } from '@/constants/Types'
-import { getValue, setValue } from '@/utils/async-storage'
-import { returnFlag, setButtonBg } from '@/utils/functions'
+import { setValue } from '@/utils/async-storage'
 import useStoredData from '@/utils/useStoredData'
 import useStore from '@/utils/zustand'
-
-function HomeOption({
-  lang,
-  onPress,
-  isSelected,
-  theme,
-}: {
-  lang: homeLanguages
-  onPress: () => void
-  isSelected: boolean
-  theme: 'light' | 'dark'
-}) {
-  const text = lang === 'pl' ? 'Polski' : 'English'
-  return (
-    <Pressable
-      onPress={onPress}
-      style={({ pressed }) => [
-        styles.option,
-        {
-          elevation: isSelected ? 1 : 0,
-          backgroundColor: setButtonBg(isSelected, pressed, theme),
-        },
-      ]}
-    >
-      <View style={styles.optionView}>
-        {returnFlag(lang)}
-        <Text style={{ fontWeight: 'bold' }}>{text}</Text>
-      </View>
-    </Pressable>
-  )
-}
-
-function ForeignOption({
-  lang,
-  onPress,
-  selectedForeignLanguages,
-  text,
-  theme,
-}: {
-  lang: foreignLanguages
-  onPress: () => void
-  selectedForeignLanguages: foreignLanguages[]
-  text: string
-  theme: 'light' | 'dark'
-}) {
-  return (
-    <Pressable
-      onPress={onPress}
-      style={({ pressed }) => [
-        styles.option,
-        {
-          elevation: selectedForeignLanguages.includes(lang) ? 1 : 0,
-          backgroundColor: setButtonBg(
-            selectedForeignLanguages.includes(lang),
-            pressed,
-            theme,
-          ),
-        },
-      ]}
-    >
-      <View style={styles.optionView}>
-        {returnFlag(lang)}
-        <Text style={{ fontWeight: 'bold' }}>{text}</Text>
-      </View>
-    </Pressable>
-  )
-}
 
 export default function SettingsScreen() {
   const setAppLang = useStore(state => state.setAppLang)
@@ -127,20 +57,6 @@ export default function SettingsScreen() {
     setTheme(localTheme)
   }, [localTheme])
 
-  async function handleSelectForeignLanguages(lang: foreignLanguages) {
-    let interestLanguages = await getValue(FAV_LANGUAGES_STORAGE_KEY)
-    interestLanguages ||= []
-    if (interestLanguages.includes(lang)) {
-      interestLanguages = interestLanguages.filter(
-        (item: foreignLanguages) => item !== lang,
-      )
-    } else {
-      interestLanguages.push(lang)
-    }
-    setFavLangs(interestLanguages)
-    setValue(FAV_LANGUAGES_STORAGE_KEY, interestLanguages)
-  }
-
   return (
     <ScrollView
       contentContainerStyle={[
@@ -148,111 +64,24 @@ export default function SettingsScreen() {
         { backgroundColor: Colors[theme].background },
       ]}
     >
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'transparent',
-          gap: 20,
-          margin: 20,
-        }}
-      >
-        <Text
-          type="title"
-          style={{
-            width: 'auto',
-            marginTop: 10,
-          }}
-        >
-          {translations[appLang].toggleTheme}
-        </Text>
-        <View
-          style={{
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-            height: 50,
-            width: 100,
-            borderRadius: 15,
-            overflow: 'hidden',
-          }}
-        >
-          <Pressable
-            style={{
-              flex: 1,
-              backgroundColor:
-                localTheme === 'dark'
-                  ? Colors[theme].buttonSecondary
-                  : Colors[theme].button,
-              justifyContent: 'center',
-            }}
-            onPress={() => setLocalTheme('light')}
-          >
-            <Feather
-              name="sun"
-              size={24}
-              style={{
-                alignSelf: 'center',
-                opacity: localTheme === 'dark' ? 0.5 : 1,
-              }}
-              color={Colors[theme].text}
-            />
-          </Pressable>
-          <Pressable
-            style={{
-              flex: 1,
-              backgroundColor:
-                localTheme === 'dark'
-                  ? Colors[theme].button
-                  : Colors[theme].buttonSecondary,
-              justifyContent: 'center',
-            }}
-            onPress={() => setLocalTheme('dark')}
-          >
-            <Feather
-              name="moon"
-              size={24}
-              style={{
-                alignSelf: 'center',
-                opacity: localTheme === 'dark' ? 1 : 0.5,
-              }}
-              color={Colors[theme].text}
-            />
-          </Pressable>
-        </View>
-      </View>
+      <ToggleTheme
+        appLang={appLang}
+        localTheme={localTheme}
+        setLocalTheme={setLocalTheme}
+      />
 
-      <Text type="title">{translations[appLang].chooseHomeLanguage}</Text>
-      <Text type="small">{translations[appLang].homeLanguageInfo}</Text>
+      <HomeLanguages
+        theme={theme}
+        appLang={appLang}
+        setLocalAppLang={setLocalAppLang}
+      />
 
-      {(['en', 'pl'] as homeLanguages[]).map(lang => (
-        <HomeOption
-          key={lang}
-          lang={lang}
-          onPress={() => setLocalAppLang(lang)}
-          isSelected={appLang === lang}
-          theme={theme}
-        />
-      ))}
-
-      <Text type="title">{translations[appLang].chooseForeignLanguage}</Text>
-
-      {Object.keys(translations[appLang].foreignLanguages).map(key => {
-        const langCode = key as foreignLanguages
-        let langText: string =
-          // @ts-ignore
-          translations[appLang].foreignLanguages[langCode]
-        return (
-          <ForeignOption
-            key={langCode}
-            selectedForeignLanguages={favLangs}
-            lang={langCode}
-            text={langText}
-            onPress={() => handleSelectForeignLanguages(langCode)}
-            theme={theme}
-          />
-        )
-      })}
+      <ForeignLanguages
+        theme={theme}
+        appLang={appLang}
+        favLangs={favLangs}
+        setFavLangs={setFavLangs}
+      />
     </ScrollView>
   )
 }
@@ -262,25 +91,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
     minHeight: '100%',
-  },
-  option: {
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 8,
-    // backgroundColor: '#ddd',
-    width: '80%',
-    maxWidth: 300,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: 10,
-  },
-  optionView: {
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    gap: 10,
-    alignItems: 'center',
-    borderRadius: 5,
-    width: 110,
   },
 })

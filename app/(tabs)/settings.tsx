3f9fe7ad9b1e3a8901bb751d74426cc352c09bdf
Mikołaj <kaleta222@gmail.com>
2025-02-25 17:41:07 +0100

    feat: add a button for clearing storage in dev mode

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { ScrollView, StyleSheet } from 'react-native'
 
+import Button from '@/components/Button'
 import ForeignLanguages from '@/components/settings/ForeignLanguages'
 import HomeLanguages from '@/components/settings/HomeLanguages'
 import ToggleTheme from '@/components/settings/ToggleTheme'
@@ -9,7 +10,7 @@ import {
   COLOR_THEME_STORAGE_KEY,
   HOME_LANGUAGE_STORAGE_KEY,
 } from '@/constants/StorageKeys'
-import { setValue } from '@/utils/async-storage'
+import { clearAsyncStorage, setValue } from '@/utils/async-storage'
 import useStoredData from '@/utils/useStoredData'
 import useStore from '@/utils/zustand'
 
@@ -82,6 +83,18 @@ export default function SettingsScreen() {
         favLangs={favLangs}
         setFavLangs={setFavLangs}
       />
+
+      {__DEV__ && (
+        <Button
+          text="clear async storage"
+          type="primary"
+          onPress={() => {
+            ;(async () => {
+              await clearAsyncStorage()
+            })()
+          }}
+        ></Button>
+      )}
     </ScrollView>
   )
 }
